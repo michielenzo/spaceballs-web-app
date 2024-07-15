ebfe078a610570c68b30397ea94f9d296c52a86b
Michiel Idema <michielidema@outlook.com>
2024-07-15 18:05:23 +0200

    AdSense try 3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -187,7 +187,8 @@ function App() {
   return (
       <div className="App">
         <Helmet>
-          <meta name="google-adsense-account" content="ca-pub-2164777390188769" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2164777390188769"
+                  crossOrigin="anonymous"></script>
         </Helmet>
 
         { state === GUIState.GAME_STARTED ? (
